1c2688af7c067adac1c1411eb46b021db2153c83
Darshan Ponikar <darshanp@geekyants.com>
2022-07-04 17:46:09 +0530

    feat: methods added

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { getIpAddress, getMacAddress } from 'react-native-turbo-device-info';
+import {
+  getIpAddress,
+  getMacAddress,
+  getTotalDiskCapacity,
+  isEmulator,
+  getFreeDiskStorage,
+  getCarrierSync,
+  isBatteryCharging,
+  getBatteryLevel,
+  isAirplaneMode,
+  isHeadphonesConnected,
+} from 'react-native-turbo-device-info';
 
 export default function App() {
   const [macAddress] = React.useState<string>(getIpAddress());
   const [ipAddress] = React.useState<string>(getMacAddress());
 
+  React.useEffect(() => {
+    console.log('isEmulator: ', isEmulator());
+    console.log('getTotalDiskCapacity: ', getTotalDiskCapacity());
+    console.log('getFreeDiskStorage: ', getFreeDiskStorage());
+    console.log('getCarrierSync: ', getCarrierSync());
+    console.log('isBatteryCharging: ', isBatteryCharging());
+    console.log('getBatteryLevel: ', getBatteryLevel());
+    console.log('isAirplaneMode: ', isAirplaneMode());
+    console.log('isHeadphonesConnected: ', isHeadphonesConnected());
+  }, []);
 
   return (
     <View style={styles.container}>
